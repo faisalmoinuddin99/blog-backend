aa0270b0bbd6798529ea634048d5efcb622499e9
faisalmoinuddin99 <faisal25march99@gmail.com>
2022-01-01 07:40:27 +0530

    Remove In-Memory Authentication to Database Authentication DONE

--- springboot-blog-rest-api/src/main/java/com/springboot/blog/security/CustomUserDetailsService.java
+++ springboot-blog-rest-api/src/main/java/com/springboot/blog/security/CustomUserDetailsService.java
@@ -12,7 +12,6 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
-import java.util.GregorianCalendar;
 import java.util.Set;
 import java.util.stream.Collectors;
 
